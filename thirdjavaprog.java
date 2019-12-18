23aee6029eaf0c4af9c6186483db106628efd3fc
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2019-12-18 12:57:06 +0200

    A few another logic

--- thirdjavaprog.java
+++ thirdjavaprog.java
@@ -1,10 +1,9 @@
 public class Hello {
 	public static void main(String[]args) {
-		int a = 4;
-		int b = 92;
-		double z = a%b;
-	System.out.println("Hello world "+(a+b)+"This is result mod "+z);
-//We have changes for three files. Next step add and comment that files and push
-//on the internet
+		final double pi=3.14;
+		int r = 92;
+		double result = 2*pi*r;
+	System.out.println("Radius =  "+result);
+// a few changes in program
 	}
 }
