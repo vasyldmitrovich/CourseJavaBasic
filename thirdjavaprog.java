51994e37b254cdbbad14112cc184e17566c04633
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2019-12-18 12:17:32 +0200

    Three files commit

--- thirdjavaprog.java
+++ thirdjavaprog.java
@@ -1,5 +1,10 @@
 public class Hello {
 	public static void main(String[]args) {
-	System.out.println("Hello world");
-}
+		int a = 4;
+		int b = 92;
+		double z = a%b;
+	System.out.println("Hello world "+(a+b)+"This is result mod "+z);
+//We have changes for three files. Next step add and comment that files and push
+//on the internet
+	}
 }
