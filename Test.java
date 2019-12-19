2d4c1894c888c25438c2a91a437745302510ccb2
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2019-12-19 09:31:33 +0200

    Add program where is three massage

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        String s1="Need work";
+        String s2="Need work harder";
+        String s3="Need work hardest";
+        System.out.println("Tomorrow I need: "+s1+"\nAnd need: "+s2+"\nAnd I need: "+s3);
+    }
+}
